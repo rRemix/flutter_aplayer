6c063c480d412d7d7cb2978614c6971f2fcfd63b
rRemix <remix@example.com>
2023-10-07 17:49:46 +0800

    专辑列表

--- lib/widgets/library/album_library.dart
+++ lib/widgets/library/album_library.dart
@@ -33,32 +33,42 @@ class _AlbumLibraryState extends State<AlbumLibrary> {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 4,
-          mainAxisSpacing: 4,
-          childAspectRatio: 0.8),
-      itemBuilder: (context, index) {
-        return AlbumItem(album: _albums[index]);
-      },
-      itemCount: _albums.length,
-      padding: EdgeInsets.only(top: 20),
-    );
+    final mainAxisExtent = MediaQuery.of(context);
+    return LayoutBuilder(builder: (context, constraints){
+      var extent = mainAxisExtent.size.width / 2 + 62;
+      return DecoratedBox(
+        decoration:
+        const BoxDecoration(color: Color.fromARGB(0xff, 0xf1, 0xf1, 0xf1)),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 0,
+              mainAxisSpacing: 0,
+              mainAxisExtent: extent),
+          itemBuilder: (context, index) {
+            return AlbumItem(
+              album: _albums[index],
+              index: index,
+            );
+          },
+          itemCount: _albums.length,
+        ),
+      );
+    });
   }
 }
 
 class AlbumItem extends StatelessWidget {
   final Album album;
+  final int index;
 
-  const AlbumItem({super.key, required this.album});
+  const AlbumItem({super.key, required this.album, required this.index});
 
   @override
   Widget build(BuildContext context) {
     ThemeData themeData = Theme.of(context);
 
     final cover = LayoutBuilder(builder: (context, constraints) {
-      debugPrint("minW: ${constraints.minWidth} maxW: ${constraints.maxWidth}");
       return FutureBuilder(
           future:
               Abilities.instance.queryArtwork(album.albumId, ArtworkType.ALBUM),
@@ -77,47 +87,90 @@ class AlbumItem extends StatelessWidget {
                   height: constraints.maxWidth,
                   fit: BoxFit.contain);
             }
-            return img;
+            return ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: img,
+            );
           });
     });
+
+    final left = index % 2 == 0 ? 4.0 : 2.0;
+    final right = index % 2 == 0 ? 2.0 : 4.0;
+    const top = 2.0;
 
     return InkWell(
       onTap: () {
         debugPrint("click: $album");
       },
-      child: Column(
-        children: [
-          cover,
-          Padding(
-              padding: EdgeInsets.all(8),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          album.album,
-                          style: themeData.textTheme.labelLarge,
-                          maxLines: 1,
-
+      child: Container(
+        padding: EdgeInsets.fromLTRB(left, top, right, top),
+        child: Column(
+          children: [
+            cover,
+            SizedBox(
+              height: 60,
+              child: DecoratedBox(
+                decoration: const BoxDecoration(color: Colors.white),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10, top: 4, bottom: 4),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 4,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              album.album,
+                              style: themeData.textTheme.labelLarge,
+                              maxLines: 1,
+                            ),
+                            Text(
+                              album.artist,
+                              style: themeData.textTheme.labelMedium,
+                              maxLines: 1,
+                            )
+                          ],
                         ),
-                        Text(
-                          album.artist,
-                          style: themeData.textTheme.labelMedium,
-                          maxLines: 1,
-                        )
-                      ],
-                    ),
-                    flex: 4,
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: PopupMenuButton(
+                          icon: const Icon(Icons.more_vert_rounded),
+                          onSelected: (int value) {
+                            debugPrint("select: $value");
+                          },
+                          itemBuilder: (context) {
+                            return [
+                              const PopupMenuItem(
+                                value: 0,
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.playlist_add),
+                                    Text("添加到播放列表")
+                                  ],
+                                ),
+                              ),
+                              const PopupMenuItem(
+                                value: 1,
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [Icon(Icons.delete), Text("删除")],
+                                ),
+                              )
+                            ];
+                          },
+                        ),
+                      )
+                    ],
                   ),
-                  Expanded(
-                    child: Icon(Icons.more_vert),
-                    flex: 1,
-                  )
-                ],
-              )),
-        ],
+                ),
+              ),
+            )
+            ,
+          ],
+        ),
       ),
     );
   }
