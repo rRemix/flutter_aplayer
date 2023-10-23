ad4a2a49d3226f09a0d3390e6a21ce174e4b4b51
rRemix <568920427@qq.com>
2023-10-23 22:00:07 +0800

    播放界面背景颜色动画

--- lib/widgets/playing_screen/playing_screen.dart
+++ lib/widgets/playing_screen/playing_screen.dart
@@ -24,13 +24,23 @@ class PlayingScreen extends StatefulWidget {
 
 const _defaultColor = Color.fromARGB(0xff, 0x88, 0x88, 0x88);
 
-class _PlayingScreenState extends State<PlayingScreen> {
+class _PlayingScreenState extends State<PlayingScreen>
+    with TickerProviderStateMixin {
   final AudioHandlerImpl audioHandlerImpl = GetIt.I<AudioHandlerImpl>();
   final ValueNotifier<int> _selected = ValueNotifier(0);
+  late AnimationController _controller;
 
   @override
   void initState() {
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 800));
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   Future<Color> _updatePalette(MediaItem? mediaItem) async {
@@ -66,194 +76,206 @@ class _PlayingScreenState extends State<PlayingScreen> {
         builder: (context, snapshot) {
           final mediaItem = snapshot.data;
 
+          final child = Flex(
+            direction: Axis.vertical,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8),
+                      child: Image.asset(
+                        "images/ic_playing_screen_back.png",
+                        width: 24,
+                        height: 24,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text(
+                              mediaItem?.title ?? "",
+                              style: themeData.textTheme.headlineMedium!
+                                  .copyWith(fontSize: 18),
+                              maxLines: 1,
+                            ),
+                            Text(
+                              "${mediaItem?.album}-${mediaItem?.artist}",
+                              style: themeData.textTheme.labelMedium!
+                                  .copyWith(fontSize: 14),
+                              maxLines: 1,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8),
+                      child: Image.asset(
+                        "images/ic_playing_screen_more.png",
+                        width: 24,
+                        height: 24,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 12,
+                child: PageView(
+                  onPageChanged: (int index) {
+                    _selected.value = index;
+                  },
+                  children: [
+                    CoverScreen(
+                      callback: (coverBytes) {
+                        // _updatePalette(coverBytes);
+                      },
+                    ),
+                    const LyricScreen()
+                  ]
+                      .map(
+                          (e) => LayoutBuilder(builder: (context, constraints) {
+                                return UnconstrainedBox(
+                                  child: ConstrainedBox(
+                                    constraints: BoxConstraints(
+                                        minHeight: 0,
+                                        maxHeight: constraints.maxHeight,
+                                        minWidth: 0,
+                                        maxWidth: constraints.maxWidth),
+                                    child: e,
+                                  ),
+                                );
+                              }))
+                      .toList(),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: ValueListenableBuilder<int>(
+                  builder: (context, value, child) {
+                    return Center(
+                      child: Indicator(
+                        count: 2,
+                        highLight: value,
+                      ),
+                    );
+                  },
+                  valueListenable: _selected,
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: SizedBox(
+                    height: 30,
+                    child: Seekbar(
+                      listener: (seekbar, progress, fromUser) {
+                        debugPrint("progress: $progress fromUser: $fromUser");
+                      },
+                      textStyle: const TextStyle(
+                          color: Color.fromARGB(0xff, 0x6b, 0x6b, 0x6b),
+                          fontSize: 12),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      splashRadius: 24,
+                      icon: Image.asset(
+                        "images/ic_playing_screen_mode_loop.png",
+                        width: 24,
+                        height: 24,
+                        color: themeData.primaryColor.withOpacity(0.4),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      splashRadius: 24,
+                      icon: Image.asset(
+                        "images/ic_playing_screen_previous.png",
+                        width: 24,
+                        height: 24,
+                        color: themeData.primaryColor,
+                      ),
+                    ),
+                    PlayPauseButton(
+                      initial: false,
+                      callback: (isPlay) {
+                        debugPrint("isPlay: $isPlay");
+                      },
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      splashRadius: 24,
+                      icon: Image.asset(
+                        "images/ic_playing_screen_next.png",
+                        width: 24,
+                        height: 24,
+                        color: themeData.primaryColor,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      splashRadius: 24,
+                      icon: Image.asset(
+                        "images/ic_playing_screen_list.png",
+                        width: 24,
+                        height: 24,
+                        color: themeData.primaryColor.withOpacity(0.4),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const Spacer(flex: 2)
+            ],
+          );
+
           return FutureBuilder(
               future: _updatePalette(mediaItem),
               builder: (context, snapshot) {
-                final startColor = snapshot.hasData ? snapshot.data!.withAlpha(0x7f) : Colors.white;
-                const endColor = Colors.white;
+                const surfaceColor = Colors.white;
+                final targetColor =
+                    snapshot.hasData ? snapshot.data! : Colors.white;
+                final animation =
+                    ColorTween(begin: surfaceColor, end: targetColor)
+                        .animate(_controller);
+
+                _controller.stop();
+                if (snapshot.hasData) {
+                  _controller.forward();
+                }
                 return SafeArea(
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: [startColor, endColor])),
-                    child: Flex(
-                      direction: Axis.vertical,
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(left: 8),
-                                child: Image.asset(
-                                  "images/ic_playing_screen_back.png",
-                                  width: 24,
-                                  height: 24,
-                                ),
-                              ),
-                              Expanded(
-                                flex: 1,
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 20),
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      Text(
-                                        mediaItem?.title ?? "",
-                                        style: themeData
-                                            .textTheme.headlineMedium!
-                                            .copyWith(fontSize: 18),
-                                        maxLines: 1,
-                                      ),
-                                      Text(
-                                        "${mediaItem?.album}-${mediaItem?.artist}",
-                                        style: themeData.textTheme.labelMedium!
-                                            .copyWith(fontSize: 14),
-                                        maxLines: 1,
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(right: 8),
-                                child: Image.asset(
-                                  "images/ic_playing_screen_more.png",
-                                  width: 24,
-                                  height: 24,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          flex: 12,
-                          child: PageView(
-                            onPageChanged: (int index) {
-                              _selected.value = index;
-                            },
-                            children: [
-                              CoverScreen(
-                                callback: (coverBytes) {
-                                  // _updatePalette(coverBytes);
-                                },
-                              ),
-                              const LyricScreen()
-                            ]
-                                .map((e) => LayoutBuilder(
-                                        builder: (context, constraints) {
-                                      return UnconstrainedBox(
-                                        child: ConstrainedBox(
-                                          constraints: BoxConstraints(
-                                              minHeight: 0,
-                                              maxHeight: constraints.maxHeight,
-                                              minWidth: 0,
-                                              maxWidth: constraints.maxWidth),
-                                          child: e,
-                                        ),
-                                      );
-                                    }))
-                                .toList(),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 2,
-                          child: ValueListenableBuilder<int>(
-                            builder: (context, value, child) {
-                              return Center(
-                                child: Indicator(
-                                  count: 2,
-                                  highLight: value,
-                                ),
-                              );
-                            },
-                            valueListenable: _selected,
-                          ),
-                        ),
-                        Expanded(
-                          flex: 2,
-                          child: Align(
-                            alignment: Alignment.topCenter,
-                            child: SizedBox(
-                              height: 30,
-                              child: Seekbar(
-                                listener: (seekbar, progress, fromUser) {
-                                  debugPrint(
-                                      "progress: $progress fromUser: $fromUser");
-                                },
-                                textStyle: const TextStyle(
-                                    color:
-                                        Color.fromARGB(0xff, 0x6b, 0x6b, 0x6b),
-                                    fontSize: 12),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 2,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                splashRadius: 24,
-                                icon: Image.asset(
-                                  "images/ic_playing_screen_mode_loop.png",
-                                  width: 24,
-                                  height: 24,
-                                  color:
-                                      themeData.primaryColor.withOpacity(0.4),
-                                ),
-                              ),
-                              IconButton(
-                                onPressed: () {},
-                                splashRadius: 24,
-                                icon: Image.asset(
-                                  "images/ic_playing_screen_previous.png",
-                                  width: 24,
-                                  height: 24,
-                                  color: themeData.primaryColor,
-                                ),
-                              ),
-                              PlayPauseButton(
-                                initial: false,
-                                callback: (isPlay) {
-                                  debugPrint("isPlay: $isPlay");
-                                },
-                              ),
-                              IconButton(
-                                onPressed: () {},
-                                splashRadius: 24,
-                                icon: Image.asset(
-                                  "images/ic_playing_screen_next.png",
-                                  width: 24,
-                                  height: 24,
-                                  color: themeData.primaryColor,
-                                ),
-                              ),
-                              IconButton(
-                                onPressed: () {},
-                                splashRadius: 24,
-                                icon: Image.asset(
-                                  "images/ic_playing_screen_list.png",
-                                  width: 24,
-                                  height: 24,
-                                  color:
-                                      themeData.primaryColor.withOpacity(0.4),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                        const Spacer(flex: 2)
-                      ],
-                    ),
+                  child: AnimatedBuilder(
+                    animation: animation,
+                    child: child,
+                    builder: (ctx, child) {
+                      logger.i("value: ${animation.value}");
+                      return DecoratedBox(
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                                colors: [animation.value!, surfaceColor])),
+                        // decoration: BoxDecoration(color: animation.value),
+                        child: child,
+                      );
+                    },
                   ),
                 );
               });
